e3b04c2f76c20ff7aa4c082804ceb45e9a02510f
Nikita-2007 <55892093+Nikita-2007@users.noreply.github.com>
2020-12-19 21:07:12 +0300

    Добовление прочего

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #RATORI-game
 from modules import game
-
+#from pyautogui import *
 game_version = 1 # Запрос версии с интернета
 
 if __name__ == '__main__':
     local_version = game.local_version
     if local_version < game_version:
         print("Данная версия игры усторела, нужно обновить")
+#        alert("Данная версия игры усторела, нужно обновить", "Обновление", button = "Ok")
     else:
         game.game_cycle()
--- modules/game.py
+++ modules/game.py
@@ -1,34 +1,35 @@
 import pygame
+from modules.Class1 import Class1
 
 local_version = 1
 
+class1 = Class1()
 pygame.init()
 g = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("RATORI")
-#icon = pygame.image.load("RATORI\images\icon.ico")
-#pygame.display.set_icon(icon)
-
+icon = pygame.image.load("images\Start.png")
+pygame.display.set_icon(icon)
+game_misic = pygame.mixer.music.load("sounds\Музыка.mp3")
+sound = pygame.mixer.Sound("sounds\click.wav")
 game_state = True
+image = pygame.image.load("images\Start.png")
+font = pygame.font.SysFont("serif", 32)
+msg = "Hi!"
+text = font.render(msg, True, "black")
 
 def game_cycle():
+    pygame.mixer.music.play(-1)
+    pygame.mixer.Sound.play(sound)
+
     while game_state:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        g.fill('pink')
-        #pygame.draw.line(g, 'yellow', (50, 50), (500, 100), 1)
-        #pygame.draw.aaline(g, 'yellow', (50, 150), (500, 200), 1)
-        #pygame.draw.line(g, 'red', (250, 50), (300, 200), 1)
-        #pygame.draw.rect(g, 'blue', (100, 100, 300, 300), 2)
-        # pygame.draw.arc(g, 'black', (100, 100, 300, 300), 1, 3, 1)
-        pygame.draw.ellipse(g, (255, 255 ,255), (200, 200, 400, 200), 100)
-        pygame.draw.circle(g, 'blue', (400, 300), 100, 90)
-        pygame.draw.circle(g, 'black', (400, 300), 100, 5)
-        pygame.draw.ellipse(g, 'purple', (200, 200, 400, 200), 9)
-        pygame.draw.circle(g, 'black', (400, 300), 30)
-        pygame.draw.ellipse(g, (255, 255 ,255), (380, 280, 10, 10))
+        class1.draw(g)
+        g.blit(text, (300, 400))
+        g.blit(image, (50, 50))
 
         pygame.time.Clock().tick(60)
         pygame.display.update()
