08d998b1dda371c38ea42a53da7b196d6f72843e
unknown <55892093+Nikita-2007@users.noreply.github.com>
2021-04-03 21:35:20 +0300

    изменение персонажа и отрисовка его массивом

--- modules/unit/Hero.py
+++ modules/unit/Hero.py
@@ -2,23 +2,32 @@ import pygame as pg
 
 class Hero(object):
     _atlas_ = pg.image.load("images\Sprite.png")
-    _rate_ = 48
+    _rate_ = 64
 
     def __init__(self, size):
         """Конструктор"""
         self.rate = self._rate_
         self.rect = pg.Rect(0, 0, self.rate, self.rate)
-
-    def update(self, e):
-        """Обновление"""
-
+        self.tile_atlas = []
+        self.tile_atlas = self.filling()
 
     def draw(self, g):
+        rect = self.rect
+        g.blit(self.tile_atlas[0][0], rect)
+        g.blit(self.tile_atlas[4][0], rect)
+        g.blit(self.tile_atlas[0][4], rect)
+        g.blit(self.tile_atlas[4][4], rect)
+
+    def filling(self):
+        """Заполняет атлас"""
         atlas = self._atlas_
         rate = self.rate
         size = (rate, rate)
-        rect = (0, 0)
-        image = atlas.subsurface(rect, size)
-        image = pg.transform.scale(image, (96, 96))
-        rect = self.rect
-        g.blit(image, rect)
+        for row in range(atlas.get_height() // rate):
+            self.tile_atlas.append([])
+            for col in range(atlas.get_width() // rate):
+                rect = (col * rate, row * rate)
+                image = atlas.subsurface(rect, size)
+                image = pg.transform.scale(image, (96, 96))
+                self.tile_atlas[row].append(image)
+        return self.tile_atlas
