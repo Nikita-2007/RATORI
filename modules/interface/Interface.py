76e72ec676a02cb48458f37ce8a99b2c53213226
unknown <55892093+Nikita-2007@users.noreply.github.com>
2021-05-22 20:58:57 +0300

    Потроны

--- modules/interface/Interface.py
+++ modules/interface/Interface.py
@@ -1,15 +1,19 @@
 from modules.interface.MiniMap import MiniMap
-
+from modules.interface.Bullet import Bullet
 class Interface(object):
 
     def __init__(self, size):
         """Конструктор"""
-        self.minimap = MiniMap(size)
+        self.size = size
+        self.minimap = MiniMap(self.size)
+        self.bullet = Bullet(self.size)
 
     def update(self, size, hero):
         """Обновление"""
         self.minimap.update(hero)
+        self.bullet.update(size)
 
     def draw(self, g):
         """Отрисовка"""
         self.minimap.draw(g)
+        self.bullet.draw(g)
